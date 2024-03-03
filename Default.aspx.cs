c46d35f1cdd18dbad52fc24b79854d798c61e13c
engineer349 <vdgopisrinivasan@gmail.com>
2024-03-03 13:44:27 +0530

    Secondcommit

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Zenhealthcare
 {
-    public partial class _Default : Page
+    public partial class Default : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
